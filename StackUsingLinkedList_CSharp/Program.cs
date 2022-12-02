bacebc5fe560e32bebd6124e5ddc585131e64f5e
ZaqyMunif <zaqymunif17@gmail.com>
2022-12-02 15:57:43 +0700

    add menu

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -16,7 +16,7 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
-    class Stack
+    class Stacks
     {
         Node top;
         public Stacks()
@@ -56,6 +56,46 @@ namespace StackUsingLinkedList_CSharp
                     Console.WriteLine(tmp.info);
                 }
                 Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n**Stack Menu**\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
             }
         }
     }
